ab22dc4a4cb86d0d4a32be806238b6c538ed3b7d
Soryn Bächli <soryn.baechli@gmail.com>
2020-01-17 14:30:41 +0100

    added needed dependencies for image generation & wallpaper set

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ColorWall
 set a Hex Code Wallpaper everyday
+
+## Reference
+![Reference](https://i.redd.it/tko26do7w9b41.png)
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+const fs = require("fs");
+const wallpaper = require("wallpaper");
+const canvas = require("canvas");
+const schedule = require("node-schedule");
